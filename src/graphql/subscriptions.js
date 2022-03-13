4e1ffcedf06663203e2942cf8c679fdf235dd42d
Yaifer <JHANNON@madisoncollege.edu>
2022-03-13 14:08:54 -0500

    aplify push

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote {
+    onCreateNote {
+      id
+      client
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote {
+    onUpdateNote {
+      id
+      client
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote {
+    onDeleteNote {
+      id
+      client
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
